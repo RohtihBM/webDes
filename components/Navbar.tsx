803ad35fdb280252286084263ae6c4b493b6fad3
RohithBM <149565715+RohtihBM@users.noreply.github.com>
2024-09-11 19:42:05 +0530

    removed logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -24,8 +24,6 @@ const Navbar = ({
 
   return (
     <nav className="flex select-none items-center justify-between gap-4 bg-primary-black px-5 text-white">
-      <Image src="/assets/logo.svg" alt="FigPro Logo" width={58} height={20} />
-
       <ul className="flex flex-row">
         {navElements.map((item: ActiveElement | any) => (
           <li
